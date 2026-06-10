bd4e62a6d261dc8efc342680d097f8a6f216ee1c
lbalog79 <loribalog@microsoft.com>
2026-06-10 17:37:39 -0400

    Improve main.tsx with error handling and Suspense fallback

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element with id 'root' not found in index.html");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Suspense>
   </React.StrictMode>
 );
